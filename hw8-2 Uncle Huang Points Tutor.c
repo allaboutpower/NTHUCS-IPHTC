650a16ea5f1b2b21adcd3a9e047adb7b6fe1a537
allaboutpower <103065775+allaboutpower@users.noreply.github.com>
2022-05-10 16:27:37 +0800

    Add files via upload

---
+++ hw8-2 Uncle Huang Points Tutor.c
@@ -0,0 +1,28 @@
+  #include<stdio.h>
+
+long long fpw(long long x, long long y, int m);
+
+int main(void){
+    long long x;
+    long long y;
+    int m;
+    scanf("%lld %lld %d", &x, &y, &m);
+    printf("%lld", fpw(x, y, m)) ;
+    return 0;
+
+
+}
+
+
+long long fpw(long long x, long long y, int m){
+    if(y == 0) return 1%m;
+    long long tmp;
+    if(y%2==0){
+        tmp = fpw(x, y/2, m);
+        return (tmp * tmp)%m; 
+    }
+    else{
+        tmp = fpw(x, y/2, m);
+        return ((tmp * tmp)%m * (x%m))%m ;
+    }
+}
